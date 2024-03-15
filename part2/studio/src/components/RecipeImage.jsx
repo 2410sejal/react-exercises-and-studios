53fcbfeaa5455e0e90b9efd6fdc3eb93af3d7174
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-03-14 23:21:09 -0500

    React Part2 Studio

--- part2/studio/src/components/RecipeImage.jsx
+++ part2/studio/src/components/RecipeImage.jsx
@@ -1,9 +1,13 @@
+import recipedata from "./recipe.json";
+import "./styling.css";
+
 function RecipeImage() {
-   return;
- }
- 
- export default RecipeImage;
- 
- //import json file for the data
- //apply css for className recipeImage
- 
+  const recipeImage = (
+    <div key={recipedata[0].name}>
+      <img src={recipedata[0].recipeImage} alt={recipedata[0].name} className="recipeImage" />
+    </div>
+  );
+  return <div>{recipeImage}</div>;
+}
+
+export default RecipeImage;
--- part2/studio/src/components/RecipeName.jsx
+++ part2/studio/src/components/RecipeName.jsx
@@ -1,9 +1,12 @@
 import recipedata from "./recipe.json";
 
 function RecipeName() {
-  return recipedata;
+  const recipeName = (
+    <div key={recipedata[0].name}>
+      <h1>{recipedata[0].name}</h1>
+    </div>
+  );
+  return <div>{recipeName}</div>;
 }
 
 export default RecipeName;
-
-//import return the name of the recipe as a level 1 header
